6e373f0b296b090e0af83b2248f82eea70f0a008
Xin Huang <xinhuang@vpn-052-107.usc.edu>
2018-12-17 17:42:03 +0800

    finish 1.1

--- Lab/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/xinhuang/Documents/cmu_self/15445/Lab
-# Build directory: /Users/xinhuang/Documents/cmu_self/15445/Lab/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("src")
-subdirs("test")
--- Lab/build/src/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/xinhuang/Documents/cmu_self/15445/Lab/src
-# Build directory: /Users/xinhuang/Documents/cmu_self/15445/Lab/build/src
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
